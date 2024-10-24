e383781f378fb7e5bde70bee2bfcc53122a88384
Paul Schuur <115781661+PaulSchuurWortell@users.noreply.github.com>
2024-10-24 12:27:31 +0200

    Add files via upload

---
+++ Get-ServerShares.ps1
@@ -0,0 +1,38 @@
+﻿class myOutput {
+    $servername
+    $sharename
+}
+
+$servers=Get-ADComputer -SearchBase "OU=,DC=,DC=" -Filter * | ? name -NotLike *nothishost*
+
+$output=@()
+
+$servers | sort name | select -First 500 | %{
+    $servername=$_.name
+    if (-not (Test-Connection -ComputerName $_.name -Count 1 -ErrorAction SilentlyContinue)) {
+        $myOutputLine=New-Object myOutput
+        $myOutputLine.sharename='(offline)'
+        $myOutputLine.servername=$servername
+        $output+=$myOutputLine
+    } else {
+        $shares=@()
+        $shares+=Get-SmbShare -CimSession $servername | ? Description -ne 'Default share' | ? Name -NotIn ('IPC$','Admin$')
+        if ($shares.count -gt 0) {
+            $shares | %{
+                $myOutputLine=New-Object myOutput
+                $myOutputLine.sharename=$_.Name
+                $myOutputLine.servername=$servername
+                $output+=$myOutputLine
+            }
+        } else {
+            $myOutputLine=New-Object myOutput
+            $myOutputLine.sharename='(none)'
+            $myOutputLine.servername=$servername
+            $output+=$myOutputLine
+        }
+    }
+
+}
+
+$output | Out-GridView
+
